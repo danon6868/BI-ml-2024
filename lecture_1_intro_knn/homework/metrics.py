a26bc19c1cf5338c8578243205f187fc9d101d26
Dorzhi Badmadashiev <dbadmadashiev@gmail.com>
2024-02-15 23:20:24 +0300

    Update .ipynb and .py

--- lecture_1_intro_knn/homework/metrics.py
+++ lecture_1_intro_knn/homework/metrics.py
@@ -16,10 +16,20 @@ def binary_classification_metrics(y_pred, y_true):
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    if not (isinstance(y_pred, (int, float)) and isinstance(y_true, (int, float))):
+        y_pred = np.array([float(x) for x in y_pred])
+        y_true = np.array([float(x) for x in y_true])
+
+    tp = np.sum((y_pred == 1) & (y_true == 1))
+    fp = np.sum((y_pred == 1) & (y_true == 0))
+    fn = np.sum((y_pred == 0) & (y_true == 1))
+
+    precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+    f1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+    accuracy = np.mean(y_pred == y_true)
+
+    return precision, recall, f1, accuracy
 
 
 def multiclass_accuracy(y_pred, y_true):
@@ -32,10 +42,11 @@ def multiclass_accuracy(y_pred, y_true):
     accuracy - ratio of accurate predictions to total samples
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    if not (isinstance(y_pred, (int, float)) and isinstance(y_true, (int, float))):
+        y_pred = np.array([float(x) for x in y_pred])
+        y_true = np.array([float(x) for x in y_true])
+        
+    return np.mean(y_pred == y_true)
 
 
 def r_squared(y_pred, y_true):
@@ -48,10 +59,11 @@ def r_squared(y_pred, y_true):
     r2 - r-squared value
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    mean_y = np.mean(y_true)
+    ss_total = np.sum((y_true - mean_y) ** 2)
+    ss_residual = np.sum((y_true - y_pred) ** 2)
+    r2 = 1 - (ss_residual / ss_total) if ss_total != 0 else 0
+    return r2
 
 
 def mse(y_pred, y_true):
@@ -64,10 +76,7 @@ def mse(y_pred, y_true):
     mse - mean squared error
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    return np.mean((y_pred - y_true) ** 2)
 
 
 def mae(y_pred, y_true):
@@ -80,8 +89,5 @@ def mae(y_pred, y_true):
     mae - mean absolut error
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    return np.mean(np.abs(y_pred - y_true))
     
